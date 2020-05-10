4daa789d490966367b7a57a70498bc2f43034e03
Sean King <sean@seanking.co>
2020-05-10 18:46:07 +1200

    add CSS mocks for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/toMatchString.ts'],
+  moduleNameMapper: {
+    '^.+\\.(css)$': '<rootDir>/.jest/mocks/css.ts'
+  },
   collectCoverage: true
 };
